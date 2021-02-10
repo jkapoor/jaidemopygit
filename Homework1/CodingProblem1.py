a56b8c4eefed5be45414a2897c1731417de4c96d
jkapoor <77764787+jkapoor@users.noreply.github.com>
2021-02-10 10:54:54 -0600

    CodingProblem1

---
+++ Homework1/CodingProblem1.py
@@ -0,0 +1,16 @@
+#Coding Problem 1 Jai Kapoor 1901832
+
+print('Birthday Calculator')
+
+print('Current day')
+month = int(input('Month: '))
+day = int(input('Day: '))
+year = int(input('Year: '))
+
+print('Birthday')
+birth_month = int(input('Month: '))
+birth_day = int(input('Day: '))
+birth_year = int(input('Year: '))
+
+age = year - birth_year
+print('You are ' + str(age) + ' years old.')
