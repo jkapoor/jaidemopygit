68d0cb982a1f58361475723ec15dedd714371ecb
jkapoor <77764787+jkapoor@users.noreply.github.com>
2021-03-30 22:57:34 -0500

    10.17.py

---
+++ Homework3/10.17.py
@@ -0,0 +1,34 @@
+#ZyLabs 10.17 Jai Kapoor 1901832
+class ItemToPurchase:
+    def __init__(self):
+        self.item_name = "none"
+        self.item_price = 0
+        self.item_quantity = 0
+
+    def print_item_cost(self):
+        print(self.item_name + " " + str(self.item_quantity) + " @ $" + str(int(self.item_price)) + " = $" +
+              str(int(self.item_price * self.item_quantity)))
+
+
+if __name__ == '__main__':
+    print("Item 1")
+    item1 = ItemToPurchase()
+    item1.item_name = input('Enter the item name:\n')
+    item1.item_price = float(input('Enter the item price:\n'))
+    item1.item_quantity = int(input('Enter the item quantity:\n'))
+
+    print()
+    print("Item 2")
+    item2 = ItemToPurchase()
+    item2.item_name = input('Enter the item name:\n')
+    item2.item_price = float(input('Enter the item price:\n'))
+    item2.item_quantity = int(input('Enter the item quantity:\n'))
+
+    print()
+    print("TOTAL COST")
+    item1.print_item_cost()
+    item2.print_item_cost()
+
+    total = (item1.item_price * item1.item_quantity) + (item2.item_price * item2.item_quantity)
+
+    print("\nTotal: $" + str(int(total)))
